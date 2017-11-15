9ee94973b931c831c47331c6b13df4fc9ec15ce9
Jorge Salas <jorge_salas@Jorges-MacBook-Pro.local>
2017-11-15 10:54:16 -0800

    phase 4 updates

--- routes/API.js
+++ routes/API.js
@@ -5,41 +5,34 @@ var waitList = require("../data/waitList.js");
 console.log(reservationList);
 console.log(waitList);
 
-// app.get("/", function(req, res) {
-//   	res.sendFile(path.join(__dirname, "index.html"));
-// });
 
-// app.get("/reserve", function(req, res) {
-//   	res.sendFile(path.join(__dirname, "reserve.html"));
-// });
+app.get("/api/tables", function(req, res) {
+  	res.json(reservationList);
+  	res.json(waitList);
 
-// app.get("/tables", function(req, res) {
-// 	res.sendFile(path.join(__dirname, "tables.html"));
-// });
+}); 
 
-// app.get("/api/tables?", function(req, res) {
-//   	res.json(reservationList);
-//   	res.json(waitList);
-
-// });
-
-// app.post("/api/new", function(req, res) {
+app.post("/api/tables", function(req, res) {
 
 
-// 	var newCustomer = req.body;
-// 	newCustomer.userID = newCustomer.name.replace(/\s+/g, "").toLowerCase();
-// 	console.log(newCustomer);
-// 	// if the reservation list is full
-// 	if (reservationList.length >=5) { 
+	var newCustomer = req.body;
+	newCustomer.userID = newCustomer.name.replace(/\s+/g, "").toLowerCase();
+	console.log(newCustomer);
+	// if the reservation list is full
+	if (reservationList.length >=5) { 
 
-// 	  	waitList.push(newCustomer);
+	  	waitList.push(newCustomer);
 
-// 	  	res.json(newCustomer);
-// 	}  
-// 	// if the reservation list is open
-// 	else {
-// 		reservationList.push(newCustomer);
-// 	}	
-// });
+	  	res.json(newCustomer);
+	}  
+	// if the reservation list is open
+	else {
+		reservationList.push(newCustomer);
+	}	
+});
 
-// module.exports = function (app);
+module.exports = function(app);
+
+
+
+
--- server.js
+++ server.js
@@ -5,11 +5,13 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-require('/routes')(app);
+require('/routes/API.js')(app);
+require('/routes/html.js')(app);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
 app.listen(PORT, function() {
   	console.log("App listening on PORT " + PORT);
-});
+}); 
